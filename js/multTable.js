9e1e005183c1c39dca62bb94a45baed141c8c737
Pratt01898630 <70974647+Pratt01898630@users.noreply.github.com>
2023-11-28 17:10:11 -0500

    Update multTable.js

--- js/multTable.js
+++ js/multTable.js
@@ -6,9 +6,9 @@ Email: aaron_pratt@student.uml.edu
 Description: Contains all java script code for recieving user input and generating multiplication table
 */
 
-// Wait for the DOM to be ready before accessing elements
+// wait for the DOM to be ready before accessing elements
 $(document).ready(function () {
-  // Get references to the HTML elements
+  // get references to the HTML elements
   const minColumnInput = $('#min-column');
   const maxColumnInput = $('#max-column');
   const minRowInput = $('#min-row');
@@ -16,21 +16,21 @@ $(document).ready(function () {
   const table = $('#multiplication-table');
   const submitButton = $('#submit-button');
 
-  // Add validation method for columns
+  // add validation method for columns
   $.validator.addMethod('minMaxColumns', function () {
     const minColumn = parseInt(minColumnInput.val());
     const maxColumn = parseInt(maxColumnInput.val());
     return minColumn <= maxColumn;
   }, 'Minimum column value must be less than or equal to maximum column value');
 
-  // Add validation method for rows
+  // add validation method for rows
   $.validator.addMethod('minMaxRows', function () {
     const minRow = parseInt(minRowInput.val());
     const maxRow = parseInt(maxRowInput.val());
     return minRow <= maxRow;
   }, 'Minimum row value must be less than or equal to maximum row value');
 
-  // Validation rules
+  // validation rules
   $("#table-form").validate({
     rules: {
       'min-column': {
@@ -77,14 +77,14 @@ $(document).ready(function () {
     }
   });
 
-  // Event listener for the button to generate the table
+  // event listener for the button to generate the table
   submitButton.on('click', function () {
-    // Manually trigger validation
+    // manually trigger validation
     $("#table-form").valid();
   });
 
   function generateTable() {
-    // Retrieve input values
+    // retrieve input values
     const minColumn = parseInt(minColumnInput.val());
     const maxColumn = parseInt(maxColumnInput.val());
     const minRow = parseInt(minRowInput.val());
@@ -92,35 +92,35 @@ $(document).ready(function () {
 
     table.html('');
 
-    // Generate table only if the form is valid
+    // generate table only if the form is valid
     if ($("#table-form").valid()) {
       for (let i = minRow - 1; i <= maxRow; i++) {
-        // Creates a new row
+        // creates a new row
         const row = $('<tr>');
   
         for (let j = minColumn - 1; j <= maxColumn; j++) {
-          // Creates a new element in the table
+          // creates a new element in the table
           const element = $('<td>');
   
           if (i === minRow - 1 && j === minColumn - 1) {
-            // Set the top-left cell to empty
+            // set the top-left cell to empty
             element.text('');
           } else if (i === minRow - 1) {
-            // If in the first row (except the top-left cell), set to j
+            // if in the first row (except the top-left cell), set to j
             element.text(j);
           } else if (j === minColumn - 1) {
-            // If in the first column (except the top-left cell), set to i
+            // if in the first column (except the top-left cell), set to i
             element.text(i);
           } else {
-            // Set other cells to i * j
+            // set other cells to i * j
             element.text(i * j);
           }
   
-          // Add element to row
+          // add element to row
           row.append(element);
         }
   
-        // Add row to table
+        // add row to table
         table.append(row);
       }
     }
